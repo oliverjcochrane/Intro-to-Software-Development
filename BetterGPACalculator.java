416c0069310ed9fe6151649cf7a8c9f47e86eb3f
Oliver Cochrane <100448572+oliverjcochrane@users.noreply.github.com>
2023-02-25 21:11:30 -0500

    Conditional converter update
    A better version of the GPA calculator. Now includes conditionals to convert letter grades to numeric doubles.

--- BetterGPACalculator.java
+++ BetterGPACalculator.java
@@ -1,56 +1,138 @@
 import java.util.Scanner;
 /**
- * Write a description of class BetterGPACalculator here.
+ * Improved version of a GPA calculator that converts user-input letter grades to numeric doubles.
  *
  * @author (Oliver Cochrane)
- * @version (1/25/23)
+ * @version (2/25/23)
  */
 public class BetterGPACalculator
 {
-    public static void main(String[] args) {
-
-        //INITIALIZES SCANNER OBJECT TO READ INPUT FROM THE TERMINAL
-        Scanner in = new Scanner(System.in);
-
-        //CLASS ONE
-        System.out.print("Enter the number of credits for class one: " );
-        int creditsOne = in.nextInt();
-        System.out.print("Enter the grade-point for class one: " );
-        double gradePoint1 = in.nextDouble();
-
-        //CLASS TWO
-        System.out.print("Enter the number of credits for class two: " );
-        int creditsTwo = in.nextInt();
-        System.out.print("Enter the grade-point for class two: " );
-        double gradePoint2 = in.nextDouble();
-
-        //CLASS THREE
-        System.out.print("Enter the number of credits for class three: " );
-        int creditsThree = in.nextInt();
-        System.out.print("Enter the grade-point for class three: " );
-        double gradePoint3 = in.nextDouble();
-
-        //CLASS FOUR
-        System.out.print("Enter the number of credits for class four: " );
-        int creditsFour = in.nextInt();
-        System.out.print("Enter the grade-point for class four: " );
-        double gradePoint4 = in.nextDouble();
-
-        //CLASS FIVE
-        System.out.print("Enter the number of credits for class five: " );
-        int creditsFive = in.nextInt();
-        System.out.print("Enter the grade-point for class five: " );
-        double gradePoint5 = in.nextDouble();
-
-        //SUM UP TOTAL CREDITS
-        int totalCredits = (creditsOne + creditsTwo + creditsThree + creditsFour + creditsFive);
-
-        //GPA CALCULATOR
-        double gpa = ((creditsOne*gradePoint1)+(creditsTwo*gradePoint2)+(creditsThree*gradePoint3)+(creditsFour*gradePoint4)+(creditsFive*gradePoint5))/totalCredits;
-
-        //FORMATTED AND PRINTED RESULT
+    private double creditsOne;
+    private double creditsTwo;
+    private double creditsThree;
+    private double creditsFour;
+    private double creditsFive;
+    
+    private double gradeValueOne;
+    private double gradeValueTwo;
+    private double gradeValueThree;
+    private double gradeValueFour;
+    private double gradeValueFive;
+    
+    Scanner in = new Scanner(System.in);
+    
+    public void getData() {
+        System.out.print("Enter the number of credits for Class 1: ");
+        creditsOne = in.nextDouble();
+        System.out.print("Enter the letter grade for Class 1: ");
+        String letter1 = in.next();
+        gradeValueOne = convertLetter(letter1);
         System.out.println();
-        System.out.println("Your GPA is: " + gpa);
-
+        
+        System.out.print("Enter the number of credits for Class 2: ");
+        creditsTwo = in.nextDouble();
+        System.out.print("Enter the letter grade for Class 2: ");
+        String letter2 = in.next();
+        gradeValueTwo = convertLetter(letter2);
+        System.out.println();
+        
+        System.out.print("Enter the number of credits for Class 3: ");
+        creditsThree = in.nextDouble();
+        System.out.print("Enter the letter grade for Class 3: ");
+        String letter3 = in.next();
+        gradeValueThree = convertLetter(letter3);
+        System.out.println();
+        
+        System.out.print("Enter the number of credits for Class 4: ");
+        creditsFour = in.nextDouble();
+        System.out.print("Enter the letter grade for Class 4: ");
+        String letter4 = in.next();
+        gradeValueFour = convertLetter(letter4);
+        System.out.println();
+        
+        System.out.print("Enter the number of credits for Class 5: ");
+        creditsFive = in.nextDouble();
+        System.out.print("Enter the letter grade for Class 5: ");
+        String letter5 = in.next();
+        gradeValueFive = convertLetter(letter5);
+        System.out.println();
+    }
+    
+    public double convertLetter(String letterGrade) {
+        double gradeValue = 0.0;
+        if (letterGrade.equals("A"))
+        {
+            gradeValue = 4.0;
+        }
+        else if (letterGrade.equals("A-"))
+        {
+            gradeValue = 3.7;
+        }
+        else if (letterGrade.equals("B+"))
+        {
+            gradeValue = 3.3;
+        }
+        else if (letterGrade.equals("B"))
+        {
+            gradeValue = 3.0;
+        }
+        else if (letterGrade.equals("B-"))
+        {
+            gradeValue = 2.7;
+        }
+        else if (letterGrade.equals("C+"))
+        {
+            gradeValue = 2.3;
+        }
+        else if (letterGrade.equals("C"))
+        {
+            gradeValue = 2.0;
+        }
+        else if (letterGrade.equals("C-"))
+        {
+            gradeValue = 1.7;
+        }
+        else if (letterGrade.equals("D+"))
+        {
+            gradeValue = 1.3;
+        }
+        else if (letterGrade.equals("D"))
+        {
+            gradeValue = 1.0;
+        }
+        else if (letterGrade.equals("D-"))
+        {
+            gradeValue = 0.7;
+        }
+        else if (letterGrade.equals("F"))
+        {
+            gradeValue = 0.0;
+        }
+        else 
+        {
+            System.out.println("Error: invalid letter grade");
+        }
+        return gradeValue;
+    }
+    
+    public double computeGPA() {
+        double totalCredits = creditsOne+creditsTwo+creditsThree+creditsFour+creditsFive;
+        double gpa = ((creditsOne*gradeValueOne)+(creditsTwo*gradeValueTwo)+(creditsThree*gradeValueThree)+(creditsFour*gradeValueFour)+(creditsFive*gradeValueFive))/totalCredits;
+        return gpa;
+    }
+    
+    public static void main(String[] args) {
+        //CREATE NEW GPA CALCULATOR OBJECT
+        BetterGPACalculator student = new BetterGPACalculator();
+        
+        //GET CLASS DATA FOR COMPUTATIONS
+        student.getData();
+        
+        //COMPUTE GPA
+        double studentGPA = student.computeGPA();
+        
+        //PRINT RESULTS WITH LABEL
+        System.out.print("Your GPA is: ");
+        System.out.printf("%.2f",studentGPA);
     }
 }
